1f46a957093c280824eea7d9ab66e4ca9089b7b2
Ariq Dhia Irfanudin <62055087+1157030004@users.noreply.github.com>
2022-11-11 16:54:58 +0700

    add bezier curve to nodes

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shadee.Dialogues
+{
+    [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue")]
+    public class Dialogue : ScriptableObject
+    {
+        [SerializeField] private List<DialogueNode> nodes = new List<DialogueNode>();
+        protected Dictionary<string, DialogueNode> nodeLookup = new Dictionary<string, DialogueNode>();
+
+
+        private void Awake() 
+        {
+#if UNITY_EDITOR
+            if(nodes.Count == 0)
+                nodes.Add(new DialogueNode());
+#endif
+
+            OnValidate();
+        }
+
+        private void OnValidate() 
+        {
+            nodeLookup.Clear();
+            foreach (DialogueNode node in GetAllNodes())
+            {
+                nodeLookup[node.uniqueID] = node;
+            }
+        }
+
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }
+
+        public DialogueNode GetRootNode()
+        {
+            return nodes[0];
+        }
+
+        public IEnumerable<DialogueNode> GetAllChildren(DialogueNode parentNode)
+        {
+            foreach(string childID in parentNode.children)
+            {
+                if(nodeLookup.ContainsKey(childID))
+                    yield return nodeLookup[childID];
+            }
+        }
+    }
+}
